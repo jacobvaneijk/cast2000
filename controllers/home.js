00109d61bbe309013dc5883a6c4cfbaef6830bea
Jacob van Eijk <jacob.vaneijk@gmail.com>
2016-12-23 17:22:56 +0100

    Add title to homepage route.

--- controllers/home.js
+++ controllers/home.js
@@ -4,5 +4,7 @@
  * This is the home page; all magic happens here.
  */
 exports.index = function(req, res) {
-    res.render('index.html');
+    res.render('index.html', {
+        title: 'Cast2000'
+    });
 };
